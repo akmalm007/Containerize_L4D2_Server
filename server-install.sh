a3b7fa3ddc9d98ec410a58440efe469978202459
akmalm007 <akmalmaulana915@gmail.com>
2026-04-24 14:34:25 +0700

    feat: adding script

--- server-install.sh
+++ server-install.sh
@@ -9,7 +9,7 @@ INSTALL_DIR=/steamcmd/l4d2
 $STEAM_CMD_LOC/steamcmd.sh +quit
 
 # Install Linux Dependecies First
-$DEPOT_CMD_LOC/DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436
+$DEPOT_CMD_LOC/DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777
 
 # Patch to make executable in stack since Docker and Modern linux prevent code execution in stack
 patchelf --clear-execstack $INSTALL_DIR/bin/libsteamvalidateuseridtickets.so
@@ -21,7 +21,7 @@ cp $STEAM_CMD_LOC/server.cfg $INSTALL_DIR/bin/server.cfg
 ls -al
 
 # Install the game server
-$DEPOT_CMD_LOC/DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777
+$DEPOT_CMD_LOC/DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436
  
 exec "$@"
 
