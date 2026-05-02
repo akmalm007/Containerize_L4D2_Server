25e53f8858a6c6447010738137df5d1d27e36e22
akmalm007 <akmalmaulana915@gmail.com>
2026-05-02 22:37:50 +0700

    feat: adding validation value

--- install-server.sh
+++ install-server.sh
@@ -2,16 +2,23 @@
 set -e
 
 INSTALL_DIR=$HOMEDIR/l4d2
-PLUGINS="https://github.com/akmalm007/L4D2_Survival_Plugins/releases/download/version/survival-plugins.tar.gz"
+L4D2_VALIDATION="false"
 
 # Install Linux Dependecies First
-DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777 -validate
+if [ ! -d $INSTALL_DIR/left4dead2 ]; then
+    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777 -validate
+    # Patch to make executable in stack since Docker and Modern linux prevent code execution in stack
+    patchelf --clear-execstack $INSTALL_DIR/bin/libsteamvalidateuseridtickets.so
+elif [ "$L4D2_VALIDATION" = "true" ]; then
+    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777 -validate
+fi
 
-# Patch to make executable in stack since Docker and Modern linux prevent code execution in stack
-patchelf --clear-execstack $INSTALL_DIR/bin/libsteamvalidateuseridtickets.so
-
+if [ ! -d $INSTALL_DIR/left4dead2 ]; then
 # Install the game server
-DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436 -validate
+    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436 -validate
+elif [ "$L4D2_VALIDATION" = "true" ]; then
+    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436 -validate
+fi
 
 exec "$@"
 
