5380bcff51d40229cf7158b49df0e3f49226810f
akmalm007 <akmalmaulana915@gmail.com>
2026-05-06 13:46:59 +0700

    feat: change logic in l4d2 installation

--- install-server.sh
+++ install-server.sh
@@ -9,15 +9,10 @@ if [ ! -d $INSTALL_DIR/left4dead2 ]; then
     DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777 -validate
     # Patch to make executable in stack since Docker and Modern linux prevent code execution in stack
     patchelf --clear-execstack $INSTALL_DIR/bin/libsteamvalidateuseridtickets.so
+    # Dowload and install game
+    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436 -validate
 elif [ "$L4D2_VALIDATION" = "true" ]; then
     DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222863 -manifest 2405357637318523777 -validate
-fi
-
-if [ ! -d $INSTALL_DIR/left4dead2 ]; then
-# Install the game server
-    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436 -validate
-elif [ "$L4D2_VALIDATION" = "true" ]; then
-    DepotDownloader -dir $INSTALL_DIR -app 222860 -depot 222861 -manifest 4827977561765481436 -validate
 fi
 
 exec "$@"
